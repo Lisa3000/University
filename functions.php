bc633fae22c6b49ebd48010c82815bd04549d99f
Lisa Nelson <33765828+Lisa3000@users.noreply.github.com>
2019-01-18 10:55:21 -0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -46,7 +46,7 @@ add_action('rest_api_init', 'university_custom_rest');
   <?php }
 
 function university_files() {
-  wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyDmQg2HifwNvPKzr-I4Kh-li9EzJX4Z7cs', NULL, '1.0', true);
+  wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=<YOUR API KEY HERE>', NULL, '1.0', true);
   wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Rosario:400,700');
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
@@ -103,7 +103,7 @@ function university_adjust_queries($query) {
 add_action('pre_get_posts', 'university_adjust_queries');
 
 function universityMapKey($api) {
-  $api['key'] = 'AIzaSyDmQg2HifwNvPKzr-I4Kh-li9EzJX4Z7cs';
+  $api['key'] = '<YOUR API KEY HERE>';
   return $api;
 }
 
